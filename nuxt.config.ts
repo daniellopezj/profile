647f51088be42357ca757a981fcb6b014663ca9d
Daniel Lopez <daniellopezj0327@gmail.com>
2023-08-18 08:46:00 -0500

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  srcDir: 'src/',
+  routeRules: {
+    '/**': { static: true, ssr: false },
+  },
+  
+})
